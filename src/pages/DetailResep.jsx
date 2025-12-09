6900ad66db7269544d28a3daa7a71033114f1758
Rrahmania <rahmaniautami34@gmail.com>
2025-12-09 23:19:58 +0700

    Update DetailResep.jsx

--- src/pages/DetailResep.jsx
+++ src/pages/DetailResep.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import { initialRecipes } from '../data/resep';
-import { recipeService } from '../services/recipeService';
 import { getRecipeRating } from '../utils/ratingUtils';
 import { recipeService } from '../services/recipeService';
 import './DetailResep.css';
